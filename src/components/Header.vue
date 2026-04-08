60c9bdc85391beddac815a8478acde284f955a81
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2026-04-08 23:48:17 +0400

    fixed wrapper issue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,6 @@
 <template>
-    <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-    <RouterLink :to="{ name: 'courses' }">Browse Courses</RouterLink>
+    <div class="wrapper flex justify-between">
+        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+        <RouterLink :to="{ name: 'courses' }">Browse Courses</RouterLink>
+    </div>
 </template>
