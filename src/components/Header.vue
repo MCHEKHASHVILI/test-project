95d4e6f5551cdfb0e74cbf937705186d67d2fdb6
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2026-04-09 03:04:07 +0400

    restyling header section missing animations and hover effects

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,17 +8,30 @@ import StarsSVG from '@/assets/svg/Stars.svg?url';
             <RouterLink :to="{ name: 'home' }">
                 <img src="@/assets/svg/ApplicationLogo.svg" alt="Logo" class="logo" />
             </RouterLink>
-            <ul class="flex space-x-4">
-                <li>
-                    <NaviLink to="{ name: 'courses' }" title="Browse Courses" :icon="StarsSVG" alt="Browse Courses" />
-                </li>
-                <li>
-                    Login
-                </li>
-                <li>
-                    Browse Courses
-                </li>
-            </ul>
+            <section class="flex items-center gap-9">
+                <ul class="flex">
+                    <li>
+                        <NaviLink routeName="courses" title="Browse Courses" :icon="StarsSVG" alt="Browse Courses" />
+                    </li>
+                </ul>
+                <ul class="flex gap-3.75">
+                    <li>
+                        <button class="w-28.5 h-15 py-3 px-4 bg-transparent 
+                            text-xl font-medium text-brand-yellow-500 
+                            border-2 border-brand-yellow-300
+                            rounded-lg cursor-pointer">
+                            Login
+                        </button>
+                    </li>
+                    <li>
+                        <button class="w-31.25 h-15 py-3 px-4 text-grayscale-50 
+                            text-xl font-medium bg-brand-yellow-500 
+                            border-2 border-brand-yellow-300
+                            rounded-lg cursor-pointer">
+                            Sign Up</button>
+                    </li>
+                </ul>
+            </section>
         </nav>
     </header>
 </template>
--- src/components/buttons/NaviLink.vue
+++ src/components/buttons/NaviLink.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
 const props = defineProps<{
-    to: string;
+    routeName: string;
     title: string;
     icon: string;
     alt: string;
 }>();
 const iconUrl = new URL(props.icon).href;
-console.log(iconUrl);
 </script>
 
 <template>
-    <RouterLink :to="{ name: 'courses' }"
+    <RouterLink :to="{ name: routeName }"
         class="group flex items-center gap-2 p-3.75 rounded-lg text-xl font-medium text-grayscale-600 hover:text-brand-yellow-500 transition-colors">
         <div class="w-[21px] h-[25px] bg-grayscale-600 group-hover:bg-brand-yellow-500 transition-colors" :style="{
             'mask-image': `url(&quot;${iconUrl}&quot;)`,
