9650c8704d9696891f5d5b5e9a50c8b2598e4443
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2026-04-09 00:56:54 +0400

    coppied colors and adjusted topbar styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,24 @@
 <template>
-    <div class="wrapper flex justify-between">
-        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-        <RouterLink :to="{ name: 'courses' }">Browse Courses</RouterLink>
-    </div>
+    <header class="bg-grayscale-100 py-6 border-b-1 border-grayscale-200 drop-shadow-[4%]">
+        <nav class="wrapper flex justify-between items-center">
+            <RouterLink :to="{ name: 'home' }">
+                <img src="@/assets/svg/ApplicationLogo.svg" alt="Logo" class="logo" />
+            </RouterLink>
+            <ul class="flex space-x-4">
+                <li>
+                    <RouterLink :to="{ name: 'courses' }" class="flex items-center">
+                        <img src="@/assets/svg/Stars.svg" alt="Logo"
+                            class="w-[21.67px] h-[19.5px] top-[3.25px] left-[2.17px]" />
+                        Browse Courses
+                    </RouterLink>
+                </li>
+                <li>
+                    Login
+                </li>
+                <li>
+                    Browse Courses
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
