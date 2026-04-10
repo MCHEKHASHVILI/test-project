4874600c545d2d51dd1cd3bd42e313c0098909a9
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2026-04-10 14:51:08 +0400

    separated CourseCard as a component

---
+++ src/components/cards/CourseCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import Icon from '@/components/shared/Icon.vue';
+interface Course {
+    title: string;
+    image: string;
+    description: string;
+    instructor: {
+        name: string;
+    },
+    avgRating: number;
+    basePrice: number;
+}
+defineProps<{ course: Course }>();
+</script>
+<template>
+    <div class="w-full bg-grayscale-50 rounded-lg border-b border-grayscale-100 p-5">
+        <div class="flex flex-col justify-between gap-4 h-full">
+            <img class="h-65.5 w-full object-cover rounded-[10px]" :src="course.image" :alt="course.title" />
+            <div class="flex flex-row justify-between items-center">
+                <small class="text-sm font-medium text-grayscale-500 w-full">Lecturer {{ course.instructor.name
+                    }}</small>
+                <div class="flex items-center gap-1 w-full justify-end">
+                    <Icon name="Star" alt="Average Rating" class="w-4 h-4" />
+                    <span class="text-sm font-medium text-grayscale-600">{{ course.avgRating }}</span>
+                </div>
+            </div>
+            <h3 class="text-2xl font-semibold text-grayscale-900">{{ course.title }}</h3>
+            <p class="text-md font-medium text-grayscale-500">{{ course.description }}</p>
+            <div class="flex flex-row items-center justify-between">
+                <div class="flex flex-row items-center justify-between gap-2">
+                    <span class="text-xs font-medium text-grayscale-400 capitalize">starting from</span>
+                    <h2 class="text-[32px] font-semibold text-grayscale-900">${{ course.basePrice }}</h2>
+                </div>
+                <button
+                    class="py-4.25 px-6.25 rounded-lg text-xl font-medium text-grayscale-50 bg-brand-yellow-500 cursor-pointer">
+                    Details
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue';
 import apiClient from '@/api/apiClient';
 import MainSlider from '@/components/sliders/MainSlider.vue'
-import Icon from '@/components/shared/Icon.vue';
+
 const featuredCourses = ref(null);
 
 onMounted(async () => {
@@ -16,7 +16,6 @@ onMounted(async () => {
 });
 
 </script>
-
 <template>
   <MainSlider />
   <section name="featured-courses" class="wrapper mt-20 flex flex-col gap-8">
@@ -26,32 +25,7 @@ onMounted(async () => {
       </h2>
     </div>
     <div class="flex flex-row justify-between gap-6">
-      <div v-for="course in featuredCourses" :key="course.id"
-        class="w-full bg-grayscale-50 rounded-lg border-b border-grayscale-100 p-5">
-        <div class="flex flex-col justify-between gap-4 h-full">
-          <img class="h-65.5 w-full object-cover rounded-[10px]" :src="course.image" :alt="course.title" />
-          <div class="flex flex-row justify-between items-center">
-            <small class="text-sm font-medium text-grayscale-500 w-full">Lecturer {{ course.instructor.name }}</small>
-            <div class="flex items-center gap-1 w-full justify-end">
-              <Icon name="Star" alt="Average Rating" class="w-4 h-4" />
-              <span class="text-sm font-medium text-grayscale-600">{{ course.avgRating }}</span>
-            </div>
-          </div>
-          <h3 class="text-2xl font-semibold text-grayscale-900">{{ course.title }}</h3>
-          <p class="text-md font-medium text-grayscale-500">{{ course.description }}</p>
-          <div class="flex flex-row items-center justify-between">
-            <div class="flex flex-row items-center justify-between gap-2">
-              <span class="text-xs font-medium text-grayscale-400 capitalize">starting from</span>
-              <h2 class="text-[32px] font-semibold text-grayscale-900">${{ course.basePrice }}</h2>
-            </div>
-            <button
-              class="py-4.25 px-6.25 rounded-lg text-xl font-medium text-grayscale-50 bg-brand-yellow-500 cursor-pointer">
-              Details
-            </button>
-          </div>
-        </div>
-      </div>
+      <CourseCard v-for="course in featuredCourses" :key="course.id" :course="course" />
     </div>
   </section>
-
 </template>
