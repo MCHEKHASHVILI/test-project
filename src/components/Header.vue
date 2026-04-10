b082dba87d03e34e0312b2826d25602c491f1d60
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2026-04-10 22:34:04 +0400

    added all possible objects in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Icon from './shared/Icon.vue';
 import AppLink from './shared/AppLink.vue';
+import BaseAvatar from './shared/BaseAvatar.vue';
+const isAuthenticated = true
 </script>
 <template>
   <header class="py-6 border-b border-grayscale-200 drop-shadow-[4%]">
@@ -10,17 +12,29 @@ import AppLink from './shared/AppLink.vue';
         <Icon name="ApplicationLogo" width="29" height="30" viewBox="0 0 32 33" />
       </RouterLink>
       <section class="flex items-center gap-9">
-        <ul class="flex">
+        <ul class="flex flex-row gap-2">
           <li>
             <AppLink :to="{ name: 'courses' }"
-              class="w-full group flex items-center gap-2 p-3.75 rounded-lg text-xl font-medium text-grayscale-600 hover:text-brand-yellow-500 transition-colors">
-              <Icon name="Stars" width="21" height="25" viewBox="0 0 26 26"
+              class="w-full group flex items-center justify-between gap-2 p-3.75 rounded-lg text-xl font-medium text-grayscale-600 hover:text-brand-yellow-500 transition-colors">
+              <Icon name="Stars" width="26" height="26" viewBox="0 0 26 26"
                 class="[&_path]:stroke-grayscale-600! group-hover:[&_path]:stroke-brand-yellow-500!" />
               <span>Browse Courses</span>
             </AppLink>
+
+          </li>
+          <li v-if="isAuthenticated">
+            <AppLink :to="{ name: 'courses' }"
+              class="w-full group flex items-center justify-between gap-2 p-3.75 rounded-lg text-xl font-medium text-grayscale-600 hover:text-brand-yellow-500 transition-colors">
+              <Icon name="Note" width="26" height="26" viewBox="0 0 24 22"
+                class="[&_path]:stroke-grayscale-600! group-hover:[&_path]:stroke-brand-yellow-500!" />
+              <span>Enrolled Courses</span>
+            </AppLink>
           </li>
         </ul>
-        <ul class="flex gap-3.75">
+        <AppLink v-if="isAuthenticated" to="#">
+          <BaseAvatar status="away" />
+        </AppLink>
+        <ul v-if="!isAuthenticated" class="flex gap-3.75">
           <li>
             <button
               class="w-28.5 h-15 py-3 px-4 bg-transparent text-xl font-medium text-brand-yellow-500 border-2 border-brand-yellow-300 rounded-lg cursor-pointer">
---
+++ src/components/shared/BaseAvatar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AvatarBase from '@/assets/svg/AvatarBase.svg?component'
+import StatusDot from '@/assets/svg/StatusDot.svg?component'
+
+const props = defineProps({
+    status: {
+        type: String,
+        default: 'away', // 'active' | 'away' | 'busy'
+    }
+})
+
+const statusColor = {
+    active: 'text-green-500',
+    away: 'text-yellow-500', // matches your current #F4A316
+    busy: 'text-red-500',
+}
+</script>
+
+<template>
+    <div class="relative inline-block w-14 h-14">
+        <AvatarBase class="w-full h-full" />
+        <StatusDot class="absolute bottom-0 right-0 w-3.75 h-3.75" :class="statusColor[status]" />
+    </div>
+</template>
