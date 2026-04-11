73648d73f49f56d6b9ebfa4372db3e70d50d6bb4
MCHEKHASHVILI <MCHEKHASHVILI@users.noreply.github.com>
2026-04-11 21:20:43 +0000

    deploy: ba852c2efc67e9075b20d5ad901d1959ce314759

---
+++ assets/BreadCrumbArrow-DINZmMzY.js
@@ -0,0 +1,1 @@
+import{l as e,o as t,w as n}from"./runtime-core.esm-bundler-DNIVUutP.js";var r={xmlns:`http://www.w3.org/2000/svg`,width:`8`,height:`13`,fill:`none`};function i(i,a){return n(),e(`svg`,r,[...a[0]||=[t(`path`,{fill:`#666`,d:`M0 1.06 1.06 0l5.78 5.778a.996.996 0 0 1 0 1.413l-5.78 5.78L0 11.91l5.425-5.425z`},null,-1)]])}var a={render:i};export{a as default};
