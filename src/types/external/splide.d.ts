a4132f178c40359566e9360c3ee4c46cc3699d80
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2026-04-12 18:03:26 +0400

    splide bug fixing

--- src/types/external/splide.d.ts
+++ src/types/external/splide.d.ts
@@ -1,15 +1,6 @@
-// src/types/external/splide.d.ts
 declare module '@splidejs/vue-splide' {
   import { DefineComponent } from 'vue'
-
-  // Defines the expected exports as valid components to bypass the TS7016 error
   export const Splide: DefineComponent<any, any, any>
   export const SplideSlide: DefineComponent<any, any, any>
   export const SplideTrack: DefineComponent<any, any, any>
-
-  // Default export if you use app.use(VueSplide)
-  const VueSplide: {
-    install: (app: any) => void
-  }
-  export default VueSplide
 }
