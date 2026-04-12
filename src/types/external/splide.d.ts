1b88ffa0af274514c4a2ba624b77534134be0024
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2026-04-12 18:20:42 +0400

    added splide.d.ts back

---
+++ src/types/external/splide.d.ts
@@ -0,0 +1,8 @@
+declare module '@splidejs/vue-splide' {
+  import type { DefineComponent, Plugin } from 'vue'
+
+  export const Splide: DefineComponent<any, any, any>
+  export const SplideSlide: DefineComponent<any, any, any>
+  export const SplideTrack: DefineComponent<any, any, any>
+  export const VueSplide: Plugin
+}
